89830aedcfbc21269a8de0a1677b3e61580d89db
Carlos Granell <carlos.granell.canut@gmail.com>
2022-03-22 14:53:08 +0100

    update slides11 - first part

--- docs/slides/xaringan-themer.R
+++ docs/slides/xaringan-themer.R
@@ -1,14 +1,39 @@
 library(xaringanthemer)
 
-xaringanthemer::style_mono_accent(
-  base_color = "#1c5253",
-  header_font_google = google_font("Josefin Sans"),
-  text_font_google   = google_font("Montserrat", "300", "300i"),
-  code_font_google   = google_font("Fira Mono")
+# http://urbaninstitute.github.io/graphics-styleguide/
+
+colors <- c(
+  blue = "#1277a7",
+  gray = "#d2d2d2",
+  white = "#ffffff",
+  black = "#000000",
+  green = "#367b51",
+  salmon = "#eb6a60",
+  turquoise = "#4d8984",
+  cold = "#61acf0", # blue
+  fat = "#f0a561", # orange
+  acid = "#cbd20a", # lime
+  heat = "#e74a2f", # red
+  red = "#f34213",
+  purple = "#3e2f5b",
+  orange = "#ff8811",
+  yellow = "#fdbf11"
 )
 
-# style_duo_accent(
-#   primary_color = "#1381B0",
-#   secondary_color = "#FF961C",
-#   inverse_header_color = "#FFFFFF"
-# )
+xaringanthemer::style_duo(
+  # primary color
+  primary_color = "#d2d2d2",
+  header_color = "#1277a7",
+  text_color = "#000000",
+
+  # secondar y color
+  secondary_color = "#1277a7",
+  inverse_header_color = "#ffffff",
+  inverse_text_color = "#d2d2d2",
+
+  # fonts
+  header_font_google = google_font("Martel", "300", "400"),
+  text_font_google = google_font("Lato"),
+  code_font_google = google_font("Fira Mono"),
+  colors = colors,
+)
