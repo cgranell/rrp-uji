8927fce73932c2efbba0d25ca14cd2de216a0c35
Carlos Granell <carlos.granell.canut@gmail.com>
2022-03-22 01:56:20 +0100

    Formating style to Scene11

---
+++ slides/xaringan-themer.R
@@ -0,0 +1,14 @@
+library(xaringanthemer)
+
+xaringanthemer::style_mono_accent(
+  base_color = "#1c5253",
+  header_font_google = google_font("Josefin Sans"),
+  text_font_google   = google_font("Montserrat", "300", "300i"),
+  code_font_google   = google_font("Fira Mono")
+)
+
+# style_duo_accent(
+#   primary_color = "#1381B0",
+#   secondary_color = "#FF961C",
+#   inverse_header_color = "#FFFFFF"
+# )
